2d324b4ce734501ddfdf8a3733281fd4003af056
Marton Borlay <borlaym@gmail.com>
2022-10-31 20:32:40 +0100

    add search

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import Tile from './components/Tile';
 import data from './data.json'
 import { Mini } from './types';
@@ -8,15 +8,39 @@ const Container = styled.div`
   display: flex;
   align-items: center;
   flex-wrap: wrap;
+  justify-content: center;
+`
+
+const Search = styled.input`
+  width: 300px;
+  padding: 10px;
+  display: block;
+  margin: 30px auto;
+  border-radius: 10px;
+  font-size: 20px;
 `
 
 function App() {
+  const [query, setQuery] = useState("")
+
+  const handleInputChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value)
+  }, [])
+
+  const dataToShow = data
+    .sort((a, b) => a.name > b.name ? 1 : -1)
+    .filter(a => a.name.toLowerCase().includes(query.toLowerCase()))
+
   return (
-    <Container>
-      {data.sort((a, b) => a.name > b.name ? 1 : -1).map((mini: Mini) => (
-        <Tile mini={mini} key={mini.fileName} />
-      ))}
-    </Container>
+    <div>
+      <Search type="search" value={query} onChange={handleInputChange} />
+      <Container>
+        {dataToShow.map((mini: Mini) => (
+          <Tile mini={mini} key={mini.fileName} />
+        ))}
+      </Container>
+    </div>
+
   );
 }
 
--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -9,6 +9,8 @@ type Props = {
 const TileContainer = styled.div`
   width: 200px;
   height: 200px;
+  margin: 10px;
+  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
 `
 
 export default function Tile(props: Props) {
